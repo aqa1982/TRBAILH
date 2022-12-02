5bb05752dc5bcb020dc4cda77f99e9750f54d901
aqa1982 <37027856+aqa1982@users.noreply.github.com>
2022-12-02 14:18:42 +0300

    Add files via upload

---
+++ free.py
@@ -0,0 +1,104 @@
+# Decompile by Mardis (Tools By OSᗩᗰᗩ ZᗩᗰZᗩᗰ)
+# Time Succes decompile : 2022-12-02 12:49:59.606018
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+def runtxt(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+def notice():
+	print("")
+	runtxt('    Wating For Anousment And Voting In Group Stay Happy')
+	print("")
+	runtxt("\n        Admin :- OSᗩᗰᗩ")
+	print("")
+	runtxt("\033[1;32m        YouTube Channel :- AQA-1982")
+	print("")
+	runtxt("\033[1;33m        Team :- OSᗩᗰᗩ ZᗩᗰZᗩᗰ LOVER")
+	print("")
+	runtxt("\033[1;33m            🌹🌹🌹🌹🌹🌹🌹🌹🌹🌹")
+	print("")
+	runtxt("\033[1;36m          Pro Hackers Or Programers")
+	input()
+logo="""
+======================================================
+\033[1;92m  ██████  ███████  █████   ███    ███  █████
+\033[1;93m ██    ██ ██      ██   ██  ██ ████ ██ ██   ██
+\033[1;94m ██    ██ ███████ ███████  ██ ████ ██ ███████
+\033[1;95m ██    ██      ██ ██   ██  ██  ██  ██ ██   ██
+\033[1;96m   ████   ███████ ██   ██  ██      ██ ██   ██                                                                                                                                      
+=======================================================                                                                                                                                    
+"""
+os.system('clear')
+print(logo)
+print("[🔥]•••••••••••••••••{C + S}•••••••••••••••••[🔥]")
+print("")
+print ("\033[1;32m     Welcome 🤪 To my Account And Win 500 Rupees And  😊 Accounts")
+print("")
+print("        Recommend Things To Enter my Account👇")
+print("")
+print("            LIBYA OSᗩᗰᗩ ZᗩᗰZᗩᗰ LOVER 👍🔥")
+print("")
+print("\033[1;32m ! :- FOLLOW ME ON FACEBOOK MY ACCOUNT")
+print("\033[1;33m ! :- NOTE All COUNTRIES AllOWED")
+print("\033[1;35m 1 :- SUBSCRIBE LIKE AND COMMENT")
+print("\033[1;36m 2 :- JOIN MY ACCOUNT ")
+print("\033[1;37m 3 :- SEND NAME OR SCREEN SHOT SUBSCRIBE ME")
+print("")
+print("\033[1;33m   [1] Subscribe Our Channel")
+Ahmad = input()
+if Ahmad in ["", " "]:
+    print (" [!] Please Select Correct Option")
+    exit()
+elif Ahmad in ["1", "01"]:
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100024557781911')
+    time.sleep(1.0)
+    os.system('clear')
+    print(logo)
+print("[🔥]••••••••••••{YT CHAND TRICKER}••••••••••••[🔥]")
+print("")
+print("        Recommend Things To Enter my Account👇")
+print("")
+print("\033[1;35m 1 :- subscribe like and comment")
+print("\033[1;36m 2 :- join my Account ")
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe me")
+print("")
+print("\033[1;36m   [2] Join My Account")
+Ahmad = input()
+if Ahmad in [""]:
+    print (" [!] Please Select Correct Option")
+    exit()
+elif Ahmad in ["2", "02"]:
+	os.system('xdg-open https://www.facebook.com/profile.php?id=100024557781911')
+	time.sleep(1.0)
+	os.system('clear')
+	print(logo)
+print("[🔥]•••••••••••••••••{C + S}•••••••••••••••••[🔥]")
+print("")
+print("        Recommend Things To Enter my Account👇")
+print("")
+print("\033[1;35m 1 :- subscribe like and comment")
+print("\033[1;36m 2 :- join my group ")
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe me")
+print("")
+print("\033[1;32m   [?] Enter Your Name And Send Me")
+ah = input('\n\033[1;31m    Type Your Name :-\033[1;33m ')
+tks = 'Dear%20Sir,%20Please%20Add%20My%20Name%20In%20Account%20%20%20%20%20My%20%20Name%20%20:%20'+ah
+os.system('am start https://wa.me/+218912537778?text=' + tks)
+time.sleep(1.0)
+print('\n\033[1;32m   Congratulations Your Name Enter To My Account ')
+print("")
+input("            \033[1;33m     Press Enter")
+os.system('clear')
+print(logo)
+
+notice()
